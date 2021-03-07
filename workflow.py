f0a4ed2fae0021e85748cf9ace2826707a84499d
Janne Engestoft <201708061@post.au.dk>
2021-03-07 15:15:39 +0100

    Added for-loop of ks

--- workflow.py
+++ workflow.py
@@ -3,36 +3,45 @@ from scripts.modpath import modpath
 
 gwf = Workflow()
 
-def vcf_filter(vcf_file):
-    output_vcf = f'steps/vcf_filtered/.recode.vcf'
+# Chromosome and group to perform admixture analysis on
+chr = 7
+pop = 'females'
+ks = range(4, 11)
+
+def vcf_filter(vcf_file, chrom, popfile, pop):
+    output_vcf = f'steps/recode_vcf/chr{chrom}_{pop}.recode.vcf'
+    base_name = modpath(output_vcf, suffix=('.recode.vcf', ''))
     
     inputs = [vcf_file]
     outputs = [output_vcf]
     options = {
-        'memory': '1g',
-        'walltime': '01:00:00'
+        'memory': '2g',
+        'walltime': '02:00:00'
     }
     
     spec = f'''
 
-    vcftools --gzvcf {vcf_file} --keep  \
-        --out {output_vcf}
+    mkdir -p steps/recode_vcf
+
+    vcftools --gzvcf {vcf_file} --recode --keep data/{popfile} \
+        --out {base_name}
     
     '''
 
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
-def vcf2bed(filtered_vcf, chrom):
-    bed = f'steps/plink/baboons.chr{chrom}.bed'
+def vcf2bed(chrom, pop):
+    filtered_vcf = f'steps/recode_vcf/chr{chrom}_{pop}.recode.vcf'
+    bed = f'steps/plink/chr{chrom}_{pop}.bed'
     base_name = modpath(bed, suffix=('.bed', ''))
-    pruned_bed = f'steps/plink/baboons.chr{chrom}.pruned.bed'
+    pruned_bed = f'steps/plink/chr{chrom}_{pop}.pruned.bed'
     
     inputs = [filtered_vcf]
     outputs = [pruned_bed]
 
     options = {
         'memory': '2g',
-        'walltime': '01:00:00'
+        'walltime': '02:00:00'
     }
 
     spec = f'''
@@ -50,11 +59,11 @@ def vcf2bed(filtered_vcf, chrom):
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-def admixture(k, chrom):
-    bedfile = f'steps/plink/baboons.chr{chrom}.pruned.bed'
-    outputq = f'steps/admixture/baboons.chr{chrom}.pruned.{k}.Q'
-    outputp = f'steps/admixture/baboons.chr{chrom}.pruned.{k}.P'
-    no_path = f'baboons.chr{chrom}.pruned.{k}'
+def admixture(k, chrom, pop):
+    bedfile = f'steps/plink/chr{chrom}_{pop}.pruned.bed'
+    outputq = f'results/admixture/chr{chrom}_{pop}.pruned.{k}.Q'
+    outputp = f'results/admixture/chr{chrom}_{pop}.pruned.{k}.P'
+    no_path = f'chr{chrom}_{pop}.pruned.{k}'
 
     inputs = [bedfile]
     outputs = [outputq, outputp]
@@ -66,36 +75,40 @@ def admixture(k, chrom):
 
     spec = f'''
 
-    mkdir -p steps/admixture
+    mkdir -p results/admixture
 
     admixture {bedfile} {k}
 
-    mv {no_path}* steps/admixture/
+    mv {no_path}* results/admixture/
 
     '''
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
-"""
 gwf.target_from_template(
-    name='exctract_window',
+    name='exctract_pop',
     template=vcf_filter(
-        vcf_file='../../../../primatediversity/people/kmt/baboon_flagship/steps/merged_vcfs/chr10.vcf.gz'
+        vcf_file='../../../../primatediversity/data/PG_baboons_pananu3_23_2_2021/output.filtered.snps.chr7.removed.AB.pass.vep.vcf.gz',
+        chrom=chr,
+        popfile='females_all.txt',
+        pop=pop
     )
 )
-"""
+
 
 gwf.target_from_template(
     name='vcf2bed',
     template=vcf2bed(
-        filtered_vcf='../../../../primatediversity/data/PG_baboons_pananu3_23_2_2021/output.filtered.snps.chr7.removed.AB.pass.vep.vcf.gz',
-        chrom='7'
+        chrom=chr,
+        pop=pop
     )
 )
 
-gwf.target_from_template(
-    name='admixture',
-    template=admixture(
-        k=6,
-        chrom='7'
+for k in ks:
+    gwf.target_from_template(
+        name=f'admixture_{k}',
+        template=admixture(
+            k=k,
+            chrom=chr,
+            pop=pop
+        )
     )
-)
